e6753b6af0fc06efb16fa7854e75b62b1128d148
pedroliborio <pedroliborioln@gmail.com>
2017-08-03 13:55:49 -0300

    First Commit

---
+++ src/AppLayer/MyAppLayer.cc
@@ -0,0 +1,86 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "MyAppLayer.h"
+
+Define_Module(MyAppLayer);
+
+void MyAppLayer::initialize(int stage) {
+    BaseWaveApplLayer::initialize(stage);
+    if (stage == 0) {
+        //Initializing members and pointers of your application goes here
+        EV << "Initializing " << par("appName").stringValue() << std::endl;
+    }
+    else if (stage == 1) {
+        //Initializing members that require initialized other modules goes here
+
+    }
+}
+
+void MyAppLayer::finish() {
+    BaseWaveApplLayer::finish();
+    //statistics recording goes here
+
+}
+
+void MyAppLayer::onBSM(BasicSafetyMessage* bsm) {
+    //Your application has received a beacon message from another car or RSU
+    //code for handling the message goes here
+
+}
+
+void MyAppLayer::onWSM(WaveShortMessage* wsm) {
+    //Your application has received a data message from another car or RSU
+    //code for handling the message goes here, see TraciDemo11p.cc for examples
+
+}
+
+void MyAppLayer::onWSA(WaveServiceAdvertisment* wsa) {
+    //Your application has received a service advertisement from another car or RSU
+    //code for handling the message goes here, see TraciDemo11p.cc for examples
+
+}
+
+void MyAppLayer::handleSelfMsg(cMessage* msg) {
+    switch (msg->getKind()) {
+        case SEND_BEACON_EVT: {
+            BasicSafetyMessage* bsm = new BasicSafetyMessage();
+            populateWSM(bsm);
+            sendDown(bsm);
+            scheduleAt(simTime() + beaconInterval, sendBeaconEvt);
+            break;
+        }
+        case SEND_WSA_EVT:   {
+            WaveServiceAdvertisment* wsa = new WaveServiceAdvertisment();
+            populateWSM(wsa);
+            sendDown(wsa);
+            scheduleAt(simTime() + wsaInterval, sendWSAEvt);
+            break;
+        }
+        default: {
+            if (msg)
+                DBG_APP << "APP: Error: Got Self Message of unknown kind! Name: " << msg->getName() << endl;
+            break;
+        }
+    }
+
+}
+
+void MyAppLayer::handlePositionUpdate(cObject* obj) {
+    BaseWaveApplLayer::handlePositionUpdate(obj);
+    //the vehicle has moved. Code that reacts to new positions goes here.
+    //member variables such as currentPosition and currentSpeed are updated in the parent class
+
+}
